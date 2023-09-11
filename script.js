9b9cc9539f609b00bfcf6c6ab97ae579bcd6eccf
andressa <andbrandsym@gmail.com>
2023-09-11 17:04:10 -0300

    Projeto-Conversor de money

---
+++ script.js
@@ -0,0 +1,84 @@
+
+const convertButton = document.querySelector(".convert-button")
+const currencySelect = document.querySelector(".currency-select")
+
+
+function convertValues() {
+    const inputCurrencyValue = document.querySelector(".input-currency").value
+    const currencyValueToConvert = document.querySelector(".currency-value-to-convert")
+    const currencyValueconverted = document.querySelector(".currency-value")
+
+
+    console.log(currencySelect.value)
+    const dolarToday = 5.2
+    const euroToday = 6.2
+
+
+
+
+    if (currencySelect.value == "dolar") {
+        currencyValueconverted.innerHTML = new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "USD"
+
+        }).format(inputCurrencyValue / dolarToday
+        )
+
+    }
+
+
+    if (currencySelect.value == "euro") {
+        currencyValueconverted.innerHTML =
+            new Intl.NumberFormat("de-DE", {
+                style: "currency",
+                currency: "EUR"
+
+            }).format(inputCurrencyValue / euroToday)
+
+    }
+
+
+    currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
+        style: "currency",
+        currency: "BRL"
+
+    }).format(inputCurrencyValue)
+
+}
+
+
+
+function changeCurrency() {
+    const currencyName = document.getElementById("currency-name")
+    const currencyImage = document.querySelector(".currency-img")
+
+
+    if (currencySelect.value == "dolar") {
+        currencyName.innerHTML = "Dolar Americano"
+     
+        
+           }
+
+
+
+    if (currencySelect.value == "euro") {
+        currencyName.innerHTML = "Euro"
+    
+    
+    
+    
+    }
+
+
+
+
+
+}
+
+currencySelect.addEventListener("change", changeCurrency)
+convertButton.addEventListener("click", convertValues)
+
+
+
+
+
